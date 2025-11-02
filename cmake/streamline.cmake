3d26dda5a20e4f96c306e4afd695d61db153fc5f
Sergey Permyakov <serg.permyakov@gmail.com>
2025-11-01 21:47:36 -0700

    headers only

--- cmake/streamline.cmake
+++ cmake/streamline.cmake
@@ -1,13 +1,11 @@
 include(FetchContent)
 
-# Streamline
+# Streamline - headers only
 FetchContent_Declare(
         streamline
         GIT_REPOSITORY "https://github.com/NVIDIAGameWorks/Streamline.git"
         GIT_TAG "${STREAMLINE_VERSION}"
 )
 message("Fetching streamline ${STREAMLINE_VERSION}")
-FetchContent_MakeAvailable(streamline)
-#set_property(TARGET streamline PROPERTY CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
-#set_property(TARGET streamline PROPERTY STREAMLINE_IMPORT_AS_INTERFACE ON)
+FetchContent_Populate(streamline)
 
